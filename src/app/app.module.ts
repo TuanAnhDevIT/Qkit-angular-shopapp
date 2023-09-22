f343c532e9559aebe43f093dbb06923d9f68f5d2
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-09-23 01:20:44 +0700

    feat(): design order page, confirm order page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 
 @NgModule({
   declarations: [
 
     HomeComponent,
-     HeaderComponent,
-     FooterComponent
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    DetailProductComponent,
+    OrderConfirmComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [
+    // HomeComponent,
+    // DetailProductComponent,
+    // OrderComponent,
+    OrderConfirmComponent
+  ]
 })
 export class AppModule { }
